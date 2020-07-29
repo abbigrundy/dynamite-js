54d5d6d0f42f8b3661e6fa9c41847ceb49488c7b
Abigail Grundy <abbi.grundy@bbc.co.uk>
2020-07-29 10:27:41 +0100

    modified bot with different stragies for play

--- bot.js
+++ bot.js
@@ -1,7 +1,41 @@
+const  possibleMoves = ["R", "P", "S"];
+
 class Bot {
+  constructor() {
+    this.dynamiteUsed = 0;
+  }
+
   makeMove(gamestate) {
-    return "R";
+    if (gamestate.rounds.length === 0) {
+      return this.generateRandomMove();
+    }
+
+    const lastRoundPlayed = gamestate.rounds.slice(-1)[0];
+
+    if (lastRoundPlayed.p1 === lastRoundPlayed.p2 && this.dynamiteUsed < 100) {
+      this.dynamiteUsed++;
+      return "D";
+    }
+    if (lastRoundPlayed.p1 === lastRoundPlayed.p2 && this.dynamiteUsed >= 100) {
+      return "W";
+    }
+
+    const possibleFilteredMoves = possibleMoves.filter(
+      (move) => lastRoundPlayed.p1 !== move
+    );
+    const nextMove =
+      possibleFilteredMoves[
+        Math.floor(Math.random() * possibleFilteredMoves.length)
+      ];
+
+    return nextMove;
+  }
+
+  generateRandomMove() {
+    const move = possibleMoves[
+      Math.floor(Math.random() * possibleMoves.length)
+    ];
+    return move;
   }
 }
-
 module.exports = new Bot();
